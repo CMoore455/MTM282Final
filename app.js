a7bed5515a53f716dca1d8329e6bd3eba6bf9d49
Christian Moore <codeyellow97@gmail.com>
2019-08-22 11:44:25 -0600

    Added schema for users documents and installed bcryptjs

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const session = require('express-session')
 const app = Express()
 const port = 3000
 
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://webfinaluser:<password>@number2mylord-hy3vz.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
 app.use(express.static(`${__dirname}`))
 app.use('view engine', 'pug')
 
