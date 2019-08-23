31d0c4609b8c9a331132414b3e5f59a61eb35be2
= <=>
2019-08-22 22:49:11 -0600

    debug module.exports for model file. add logic for home route and home.pug

--- routes/siteRoutes.js
+++ routes/siteRoutes.js
@@ -2,4 +2,13 @@ const express = require('express')
 const models = require('../data/models')
 const siteRouter = express.Router()
 
+siteRouter.route(['/', '/home']).get(
+    function(request, response) {
+        let model = models.getUiModel("Mirrors - Home", "Welcome to the Home Page")
+        model.user = null;
+
+        response.render("home", model )
+    }
+)
+
 module.exports = siteRouter
