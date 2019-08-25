6d88566fdd50cd7d050bb21aa2f1416b3d1c0c28
Christian Moore <codeyellow97@gmail.com>
2019-08-24 23:38:58 -0600

    added questions to registration page. Added functionality to save question responses

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -49,13 +49,15 @@ adminRouter.route('/register').get(
     function (request, response) {
         let model = models.getUiModel("Mirrors - Register", "User Registration", request)
         models.Question.find({}, function (err, docs) {
-            
-            model.questions = docs
-            if (errorMessage) {
-                model.errorMessage = errorMessage
-                errorMessage = null
+            if(docs.length){
+                model.questions = docs
+                if (errorMessage) {
+                    model.errorMessage = errorMessage
+                    errorMessage = null
+                }
+                response.render("register", model)
             }
-            response.render("register", model)
+            
         })
        
     }
@@ -65,6 +67,16 @@ adminRouter.route('/register').post(
     function (request, response) {
         var salt = bcrypt.genSaltSync(10);
         var hash = bcrypt.hashSync(request.body.password, salt);
+        var numOfQuestions = 3
+        var userResponses = []
+        for (let i = 0; i < numOfQuestions; i++){
+            splitArray = request.body["question"+i].split(":")
+            userResponses.push({
+                question: splitArray[0],
+                choice: splitArray[1]
+            })
+        }
+        console.log(userResponses)
         let newUser = new models.User(
             {
                 username: request.body.username,
@@ -72,7 +84,8 @@ adminRouter.route('/register').post(
                 email: request.body.email,
                 isAdmin: false,
                 isActive: true,
-                age: request.body.age
+                age: request.body.age,
+                responses: userResponses
             }
         )
         models.User.find( { $or: [{email: newUser.email}, {username: newUser.username}] }, function (err, docs) {
