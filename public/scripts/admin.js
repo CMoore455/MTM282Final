319924ca6b798c38895e79dd731468825a304d70
= <=>
2019-08-27 01:09:21 -0600

    end of day commit...

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,5 +1,1 @@
 console.log("admin script loaded")
-
-function updateUser() {
-    
-}
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -162,5 +162,34 @@ adminRouter.route('/logout').get(
         response.render("logout", model)
 })
 
+function updateIsActive(user_id) {
+    console.log("updating user active status")
+
+    // find user by _id
+    models.User.find( { _id: user_id } , function (err, user) {
+        if (err) return console.log(err)
+
+        user.isActive = !user.isActive
+        user.save(function(err){
+            if (err) return console.log(err)
+            console.log(`updated user to: isActive=${user.isActive}`)
+        })
+    })
+
+}
+
+function updateIsAdmin(user_id) {
+    console.log("updating user admin status")
+    // find user by _id
+    models.User.find( { _id: user_id } , function (err, user) {
+        if (err) return console.log(err)
+
+        user.isAdmin = !user.isAdmin
+        user.save(function(err){
+            if (err) return console.log(err)
+            console.log(`updated user to: isAdmin=${user.isAdmin}`)
+        })
+    })
+}
 
 module.exports = adminRouter
